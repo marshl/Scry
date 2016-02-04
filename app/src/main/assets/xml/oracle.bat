cfc10eadaca1d68154964d9b209606da5cb6e447
marshl <marshl@live.com.au>
2016-02-04 23:00:12 +1100

    Changed the xml update bat to also update the set list;

--- app/src/main/assets/xml/oracle.bat
+++ app/src/main/assets/xml/oracle.bat
@@ -1,1 +1,3 @@
-call php -q C:\wamp\www\delverdb\xmlconverter.php > oracle.xml
+@ECHO OFF
+call php -q C:\wamp\www\delverdb\xmlconverter.php oracle > oracle.xml
+call php -q C:\wamp\www\delverdb\xmlconverter.php setlist > setlist.xml
